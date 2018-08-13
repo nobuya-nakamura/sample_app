77abe268df54513fea6157c012d6e327b09dd10d
EC2 Default User <ec2-user@ip-172-31-11-198.us-east-2.compute.internal>
2018-08-13 00:59:52 +0000

    Add user microposts

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,11 @@ SampleApp::Application.routes.draw do
     :registrations => "registrations"
   }
   resources :users, only: [:show, :index, :destroy]
-  root  'static_pages#home'
+  resources :microposts, only: [:create, :destroy]
   
   #devise_for :users
   #devise_for :users
-  
+  root 'static_pages#home'
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
