d673db16e51368cc4dc70b41fb80d38282943c4d
EC2 Default User <ec2-user@ip-172-31-11-198.us-east-2.compute.internal>
2018-08-13 21:30:38 +0000

    Finish user following

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,6 +20,11 @@ class User < ActiveRecord::Base
    def unfollow!(other_user)
     relationships.find_by(followed_id: other_user.id).destroy
   end
+  
+    def feed
+    Micropost.from_users_followed_by(self)
+  end
+  
 
   has_many :reverse_relationships, foreign_key: "followed_id",
                                    class_name:  "Relationship",
